2fc481e911a876dad5068bb9a8e0329472f4cb02
Sameera <sameira@gmail.com>
2018-01-10 21:53:44 +0530

    Correcting merge

--- src/main/java/com/sdnelson/msc/research/lcf4j/nodemgmt/websocksts/client/WebSocketClient.java
+++ src/main/java/com/sdnelson/msc/research/lcf4j/nodemgmt/websocksts/client/WebSocketClient.java
@@ -18,7 +18,6 @@ package com.sdnelson.msc.research.lcf4j.nodemgmt.websocksts.client;
 import com.sdnelson.msc.research.lcf4j.core.NodeData;
 import com.sdnelson.msc.research.lcf4j.core.NodeRegistry;
 import com.sdnelson.msc.research.lcf4j.nodemgmt.websocksts.server.WebSocketServer;
-import com.sdnelson.msc.research.lcf4j.websocksts.server.WebSocketServer;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -29,6 +28,7 @@ import io.netty.handler.codec.http.HttpClientCodec;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakerFactory;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketVersion;
 import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketClientCompressionHandler;
 import io.netty.handler.ssl.SslContext;
@@ -62,7 +62,6 @@ import java.util.Properties;
  */
 public final class WebSocketClient {
 
-    final static org.apache.log4j.Logger logger = Logger.getLogger(WebSocketServer.class);
     final static org.apache.log4j.Logger logger = Logger.getLogger(WebSocketClient.class);
     static final String URL = System.getProperty("url", "ws://127.0.0.1:8080/websocket");
 
@@ -109,7 +108,6 @@ public final class WebSocketClient {
         EventLoopGroup group = new NioEventLoopGroup(Integer.valueOf(
                 properties.getProperty("client.salve.threadpool.size")));
         logger.info(properties.getProperty("client.slave.threadpool.size"));
-        EventLoopGroup group = new NioEventLoopGroup(Integer.parseInt(properties.getProperty("client.slave.threadpool.size")));
         ChannelFuture sync;
         try {
             // Connect with V13 (RFC 6455 aka HyBi-17). You can change it to V08 or V00.
@@ -122,21 +120,21 @@ public final class WebSocketClient {
 
             Bootstrap b = new Bootstrap();
             b.group(group)
-             .channel(NioSocketChannel.class)
-             .handler(new ChannelInitializer<SocketChannel>() {
-                 @Override
-                 protected void initChannel(SocketChannel ch) {
-                     ChannelPipeline p = ch.pipeline();
-                     if (sslCtx != null) {
-                         p.addLast(sslCtx.newHandler(ch.alloc(), host, port));
-                     }
-                     p.addLast(
-                             new HttpClientCodec(),
-                             new HttpObjectAggregator(8192),
-                             WebSocketClientCompressionHandler.INSTANCE,
-                             handler);
-                 }
-             });
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ChannelPipeline p = ch.pipeline();
+                            if (sslCtx != null) {
+                                p.addLast(sslCtx.newHandler(ch.alloc(), host, port));
+                            }
+                            p.addLast(
+                                    new HttpClientCodec(),
+                                    new HttpObjectAggregator(8192),
+                                    WebSocketClientCompressionHandler.INSTANCE,
+                                    handler);
+                        }
+                    });
             logger.info("Client boot sequence initiated.");
             Channel ch = b.connect(uri.getHost(), port).sync().channel();
             logger.info("Client boot sequence completed.");
@@ -148,20 +146,21 @@ public final class WebSocketClient {
                 WebSocketFrame frame = new TextWebSocketFrame(ch.localAddress().toString());
                 ch.writeAndFlush(frame);
                 Thread.sleep(1000);
-            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+                BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
 
-            while(true){
-                String hashtext = "";
-                Collection<NodeData> activeNodeDataList = NodeRegistry.getActiveNodeDataList();
-                for (NodeData dataList : activeNodeDataList) {
-                    MessageDigest md = MessageDigest.getInstance("MD5");
-                    byte[] messageDigest = md.digest(dataList.toString().getBytes());
-                    BigInteger number = new BigInteger(1, messageDigest);
-                    hashtext += number.toString(16);
+                while (true) {
+                    String hashtext = "";
+                    Collection<NodeData> activeNodeDataList = NodeRegistry.getActiveNodeDataList();
+                    for (NodeData dataList : activeNodeDataList) {
+                        MessageDigest md = MessageDigest.getInstance("MD5");
+                        byte[] messageDigest = md.digest(dataList.toString().getBytes());
+                        BigInteger number = new BigInteger(1, messageDigest);
+                        hashtext += number.toString(16);
+                    }
+                    ch.writeAndFlush(new TextWebSocketFrame("NodeData Hash : " + hashtext + ":" + " Node Count : " + NodeRegistry.getActiveNodeCount()));
+                    Thread.sleep(1000);
                 }
-                ch.writeAndFlush(new TextWebSocketFrame("NodeData Hash : " + hashtext + ":" +" Node Count : " + NodeRegistry.getActiveNodeCount()));
-                Thread.sleep(1000);
             }
 
 
@@ -203,42 +202,10 @@ public final class WebSocketClient {
 //            }
 
 
-        } finally {
-            group.shutdownGracefully();
-        }
-
-
-    }
-
-    private static Properties loadConfigFile() {
-        Properties prop = new Properties();
-        InputStream input = null;
-
-        try {
-
-            String filename = "lcf4j.properties";
-            input = WebSocketServer.class.getClassLoader().getResourceAsStream(filename);
-            if(input==null){
-                logger.error("Sorry, unable to find " + filename);
-                return null;
-            }
-
-            prop.load(input);
-            logger.info("Property file loaded successfully.");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally{
-            if(input!=null){
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    logger.error(e.getMessage());
-                }
+            } finally{
+                group.shutdownGracefully();
             }
         }
-        return prop;
-    }
 
     private static Properties loadConfigFile() {
         Properties prop = new Properties();
--- src/main/java/com/sdnelson/msc/research/lcf4j/nodemgmt/websocksts/client/WebSocketClientHandler.java
+++ src/main/java/com/sdnelson/msc/research/lcf4j/nodemgmt/websocksts/client/WebSocketClientHandler.java
@@ -35,7 +35,7 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //THE SOFTWARE.
 
-package com.sdnelson.msc.research.lcf4j.websocksts.client;
+package com.sdnelson.msc.research.lcf4j.nodemgmt.websocksts.client;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
